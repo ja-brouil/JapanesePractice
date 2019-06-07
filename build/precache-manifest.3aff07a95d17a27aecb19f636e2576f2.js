9c4aafcc5b14bf69280d8ef32bf02faec5399ff2
James Brouillet <james_brouillet@hotmail.com>
2019-06-07 14:36:44 -0700

    "Fixed a few bugs in the Kana answers"

---
+++ build/precache-manifest.3aff07a95d17a27aecb19f636e2576f2.js
@@ -0,0 +1,626 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0cb952b9fc80a19d44ca8c0bedea90",
+    "url": "/JapanesePractice/index.html"
+  },
+  {
+    "revision": "ec1c30a96e8c6302b59a",
+    "url": "/JapanesePractice/static/css/main.78235886.chunk.css"
+  },
+  {
+    "revision": "1b7752ee2bf41d7bab68",
+    "url": "/JapanesePractice/static/js/2.aa9911c9.chunk.js"
+  },
+  {
+    "revision": "ec1c30a96e8c6302b59a",
+    "url": "/JapanesePractice/static/js/main.92162bac.chunk.js"
+  },
+  {
+    "revision": "6116c481990ff73909b6",
+    "url": "/JapanesePractice/static/js/runtime~main.4d8c18d1.js"
+  },
+  {
+    "revision": "4a9d18e509d78f21f8958968c3bf06f1",
+    "url": "/JapanesePractice/static/media/a.4a9d18e5.png"
+  },
+  {
+    "revision": "e9da1ca5931f7295d08d4e8e170dd93f",
+    "url": "/JapanesePractice/static/media/a.e9da1ca5.png"
+  },
+  {
+    "revision": "05d089cb102628a84e02e9a6e9c5621c",
+    "url": "/JapanesePractice/static/media/ba.05d089cb.png"
+  },
+  {
+    "revision": "32253f32272d95d7a15ab37d379d41b1",
+    "url": "/JapanesePractice/static/media/ba.32253f32.png"
+  },
+  {
+    "revision": "59bc33c821c34ceaaca03a1001c7c128",
+    "url": "/JapanesePractice/static/media/background1.59bc33c8.jpg"
+  },
+  {
+    "revision": "8e58c9d0c45cc370250919dfd27976d9",
+    "url": "/JapanesePractice/static/media/background2.8e58c9d0.jpg"
+  },
+  {
+    "revision": "9cd4a037fea0223e892e1513c4b0d56e",
+    "url": "/JapanesePractice/static/media/background3.9cd4a037.jpg"
+  },
+  {
+    "revision": "2a477e38d94f027ba5818ba470c8af6f",
+    "url": "/JapanesePractice/static/media/background4.2a477e38.jpg"
+  },
+  {
+    "revision": "ea3a672c7219ee1eb3f83a3982652c81",
+    "url": "/JapanesePractice/static/media/background5.ea3a672c.jpg"
+  },
+  {
+    "revision": "4e5360070d6a8e5bef912ef0053b7da0",
+    "url": "/JapanesePractice/static/media/background6.4e536007.jpg"
+  },
+  {
+    "revision": "63abac7df95afbe8937166ad188d48ff",
+    "url": "/JapanesePractice/static/media/be.63abac7d.png"
+  },
+  {
+    "revision": "a74cdcaa59264b07a32351db3546abf6",
+    "url": "/JapanesePractice/static/media/be.a74cdcaa.png"
+  },
+  {
+    "revision": "aeb33354980337c3eaaeae17c2749b63",
+    "url": "/JapanesePractice/static/media/bi.aeb33354.png"
+  },
+  {
+    "revision": "17f983d257f1774f5631e7880d978161",
+    "url": "/JapanesePractice/static/media/bo.17f983d2.png"
+  },
+  {
+    "revision": "2c0193920553007c2709e096ee3e87a9",
+    "url": "/JapanesePractice/static/media/bo.2c019392.png"
+  },
+  {
+    "revision": "5576549bd6b5bbfaf81d032b0fdc635c",
+    "url": "/JapanesePractice/static/media/bu.5576549b.png"
+  },
+  {
+    "revision": "d8c8e7422cb904e828640f6be75b550e",
+    "url": "/JapanesePractice/static/media/bu.d8c8e742.png"
+  },
+  {
+    "revision": "bb13a0025d7264b36bb72d2d74a5e807",
+    "url": "/JapanesePractice/static/media/chi.bb13a002.png"
+  },
+  {
+    "revision": "e88ee4bdca2576b57a05d77a8650b647",
+    "url": "/JapanesePractice/static/media/chi.e88ee4bd.png"
+  },
+  {
+    "revision": "868f242fd9c9a46355047df020bd0218",
+    "url": "/JapanesePractice/static/media/da.868f242f.png"
+  },
+  {
+    "revision": "fb5a88ee323bd6edede3b7df447cac87",
+    "url": "/JapanesePractice/static/media/da.fb5a88ee.png"
+  },
+  {
+    "revision": "75505d26b9fae399eb588a139a382da7",
+    "url": "/JapanesePractice/static/media/de.75505d26.png"
+  },
+  {
+    "revision": "b228575798b98f7ef594fe14c7bdb4b4",
+    "url": "/JapanesePractice/static/media/de.b2285757.png"
+  },
+  {
+    "revision": "1504487c1d8ce69b28954890b8362812",
+    "url": "/JapanesePractice/static/media/do.1504487c.png"
+  },
+  {
+    "revision": "91ba81b25cc6bf1284a51b9f1b189bf9",
+    "url": "/JapanesePractice/static/media/do.91ba81b2.png"
+  },
+  {
+    "revision": "291e4528ec5d774ae538e7b2329f3eb4",
+    "url": "/JapanesePractice/static/media/e.291e4528.png"
+  },
+  {
+    "revision": "890c875f77b141af7b4dd03f6df39b34",
+    "url": "/JapanesePractice/static/media/e.890c875f.png"
+  },
+  {
+    "revision": "3582bf9d00459cda88b8cd6ad36564dd",
+    "url": "/JapanesePractice/static/media/fu.3582bf9d.png"
+  },
+  {
+    "revision": "5e76bd3af529efc84181c7b9aa8b2a20",
+    "url": "/JapanesePractice/static/media/fu.5e76bd3a.png"
+  },
+  {
+    "revision": "5e3984e02966e7f0fb180992bd970071",
+    "url": "/JapanesePractice/static/media/ga.5e3984e0.png"
+  },
+  {
+    "revision": "dcc86809e4dcaa39f0ed10c877458501",
+    "url": "/JapanesePractice/static/media/ga.dcc86809.png"
+  },
+  {
+    "revision": "49d8c5287e0ceeffd4cfe100fc5afd3b",
+    "url": "/JapanesePractice/static/media/ge.49d8c528.png"
+  },
+  {
+    "revision": "74393fa452b176bf822f794a5f525e68",
+    "url": "/JapanesePractice/static/media/ge.74393fa4.png"
+  },
+  {
+    "revision": "c817b88a7b67c665f37bd6cf6bbd16c4",
+    "url": "/JapanesePractice/static/media/gi.c817b88a.png"
+  },
+  {
+    "revision": "e718f5501ed9b558857cfedd5a3ad848",
+    "url": "/JapanesePractice/static/media/gi.e718f550.png"
+  },
+  {
+    "revision": "249bf81430bb22a8dba89484ebf9d82f",
+    "url": "/JapanesePractice/static/media/go.249bf814.png"
+  },
+  {
+    "revision": "7d490efe3de2303593c2563be14ce81a",
+    "url": "/JapanesePractice/static/media/go.7d490efe.png"
+  },
+  {
+    "revision": "8fea666eeca55f4dd00b47fd72fd81b7",
+    "url": "/JapanesePractice/static/media/gu.8fea666e.png"
+  },
+  {
+    "revision": "a71909cf5110868bae8d2aa38bc29dba",
+    "url": "/JapanesePractice/static/media/gu.a71909cf.png"
+  },
+  {
+    "revision": "47b7a5ad65913ac953b5fb04e89ee42e",
+    "url": "/JapanesePractice/static/media/ha.47b7a5ad.png"
+  },
+  {
+    "revision": "aea9fa6f35d602b61ae63fbe8e16321e",
+    "url": "/JapanesePractice/static/media/ha.aea9fa6f.png"
+  },
+  {
+    "revision": "3a27a8fdd01c799db89469a22cf969e7",
+    "url": "/JapanesePractice/static/media/he.3a27a8fd.png"
+  },
+  {
+    "revision": "a4d4a60ed388e6de804f162003182fa6",
+    "url": "/JapanesePractice/static/media/he.a4d4a60e.png"
+  },
+  {
+    "revision": "262654600482d84a1924613f640d31db",
+    "url": "/JapanesePractice/static/media/hi.26265460.png"
+  },
+  {
+    "revision": "43ca7775b83e1383d33c67cc6058776b",
+    "url": "/JapanesePractice/static/media/hi.43ca7775.png"
+  },
+  {
+    "revision": "34f1e4dbfe0a660cb2a484b1f50c8305",
+    "url": "/JapanesePractice/static/media/ho.34f1e4db.png"
+  },
+  {
+    "revision": "e374298b871a7a65bcf5e14580f4d859",
+    "url": "/JapanesePractice/static/media/ho.e374298b.png"
+  },
+  {
+    "revision": "6c3ce53d17c83bc524a8b9f536395ea4",
+    "url": "/JapanesePractice/static/media/i.6c3ce53d.png"
+  },
+  {
+    "revision": "ffae53ac167f913338f8aec25ee68f0f",
+    "url": "/JapanesePractice/static/media/i.ffae53ac.png"
+  },
+  {
+    "revision": "61f3fd2974afe1e8afe5d3e0bb459c4d",
+    "url": "/JapanesePractice/static/media/ji.61f3fd29.png"
+  },
+  {
+    "revision": "92f22945f0cafb63ef82d956b5c00a2c",
+    "url": "/JapanesePractice/static/media/ji.92f22945.png"
+  },
+  {
+    "revision": "f1126eda5cba8c150c30991ccb3e6abc",
+    "url": "/JapanesePractice/static/media/ji2.f1126eda.png"
+  },
+  {
+    "revision": "f562af958de9ce108f7c6d0c8407b40b",
+    "url": "/JapanesePractice/static/media/ji2.f562af95.png"
+  },
+  {
+    "revision": "8a5904fc3ad48d704f055d62719c4466",
+    "url": "/JapanesePractice/static/media/ka.8a5904fc.png"
+  },
+  {
+    "revision": "c5b1cb1c4820f1c56cb83a93d482f1a4",
+    "url": "/JapanesePractice/static/media/ka.c5b1cb1c.png"
+  },
+  {
+    "revision": "16b504a45ab7a555154640e1af7a40f1",
+    "url": "/JapanesePractice/static/media/ke.16b504a4.png"
+  },
+  {
+    "revision": "51dcc7ca43399bdb1c7e781f2a4c94c5",
+    "url": "/JapanesePractice/static/media/ke.51dcc7ca.png"
+  },
+  {
+    "revision": "706af255c79af72b3c41192d655bf6d2",
+    "url": "/JapanesePractice/static/media/ki.706af255.png"
+  },
+  {
+    "revision": "7182d9a7ae68176c76dff8905a7e0a67",
+    "url": "/JapanesePractice/static/media/ki.7182d9a7.png"
+  },
+  {
+    "revision": "1236660e55601c68bda628d3f4f4e272",
+    "url": "/JapanesePractice/static/media/ko.1236660e.png"
+  },
+  {
+    "revision": "2753a02c821d98670fc155608727f436",
+    "url": "/JapanesePractice/static/media/ko.2753a02c.png"
+  },
+  {
+    "revision": "4823b26202596c295116b5e04a5c8d4a",
+    "url": "/JapanesePractice/static/media/ku.4823b262.png"
+  },
+  {
+    "revision": "fafdac3742dbb2bb40fb08a1fb3e507a",
+    "url": "/JapanesePractice/static/media/ku.fafdac37.png"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/JapanesePractice/static/media/leftArrow.34a4f31c.svg"
+  },
+  {
+    "revision": "0f118e3ef943f797d0b591bd6dc83d47",
+    "url": "/JapanesePractice/static/media/ma.0f118e3e.png"
+  },
+  {
+    "revision": "ce84cacf8fdca995527cffed48e42179",
+    "url": "/JapanesePractice/static/media/ma.ce84cacf.png"
+  },
+  {
+    "revision": "294265da734b312bacdaf301d93b84c2",
+    "url": "/JapanesePractice/static/media/me.294265da.png"
+  },
+  {
+    "revision": "62c3f5296c5a3c385841b46d2632960b",
+    "url": "/JapanesePractice/static/media/me.62c3f529.png"
+  },
+  {
+    "revision": "326cb1bf19036658d9f4f56406dcef29",
+    "url": "/JapanesePractice/static/media/mi.326cb1bf.png"
+  },
+  {
+    "revision": "6a86f8df1ca2e0f85e59162f2a66916b",
+    "url": "/JapanesePractice/static/media/mi.6a86f8df.png"
+  },
+  {
+    "revision": "3415c378ce40195830f72da8a1d139cd",
+    "url": "/JapanesePractice/static/media/mo.3415c378.png"
+  },
+  {
+    "revision": "d04622623d3d911c1604f6c425c86a3b",
+    "url": "/JapanesePractice/static/media/mo.d0462262.png"
+  },
+  {
+    "revision": "74fd3c2be43e41d125c93eecbcd1bcd9",
+    "url": "/JapanesePractice/static/media/mu.74fd3c2b.png"
+  },
+  {
+    "revision": "81aeb12054010a1ad36bc864bdddfba2",
+    "url": "/JapanesePractice/static/media/mu.81aeb120.png"
+  },
+  {
+    "revision": "22b00b574b6b55a9ca6cd829e0e954ca",
+    "url": "/JapanesePractice/static/media/n.22b00b57.png"
+  },
+  {
+    "revision": "a4bb08a8e5210f9532038fabd54c6bfa",
+    "url": "/JapanesePractice/static/media/n.a4bb08a8.png"
+  },
+  {
+    "revision": "00acca1228f6e044c8e5a822ab6cbaeb",
+    "url": "/JapanesePractice/static/media/na.00acca12.png"
+  },
+  {
+    "revision": "ec85425f60d5688181e5dfca550e7d8a",
+    "url": "/JapanesePractice/static/media/na.ec85425f.png"
+  },
+  {
+    "revision": "39adb9f9904adbc8cc4a18e33ab16d6b",
+    "url": "/JapanesePractice/static/media/ne.39adb9f9.png"
+  },
+  {
+    "revision": "e48580cea44b807a59ec2e7b77c64c19",
+    "url": "/JapanesePractice/static/media/ne.e48580ce.png"
+  },
+  {
+    "revision": "0b4fcfd31cf179522dc75875a6989d56",
+    "url": "/JapanesePractice/static/media/ni.0b4fcfd3.png"
+  },
+  {
+    "revision": "a9faba498c063b26fa3e1119cf995047",
+    "url": "/JapanesePractice/static/media/ni.a9faba49.png"
+  },
+  {
+    "revision": "bd2b5d242de485ef3ff777340535a402",
+    "url": "/JapanesePractice/static/media/no.bd2b5d24.png"
+  },
+  {
+    "revision": "fd5bf857c818cbd65aa5ca4f904ae7f3",
+    "url": "/JapanesePractice/static/media/no.fd5bf857.png"
+  },
+  {
+    "revision": "46b0a29bc9860a5a71664f50a9f6b8a8",
+    "url": "/JapanesePractice/static/media/nu.46b0a29b.png"
+  },
+  {
+    "revision": "f2c773d5579ce8dcd8601b727a8bb92d",
+    "url": "/JapanesePractice/static/media/nu.f2c773d5.png"
+  },
+  {
+    "revision": "c0fe9752999f07a2f065f85ff9a606f4",
+    "url": "/JapanesePractice/static/media/o.c0fe9752.png"
+  },
+  {
+    "revision": "c83fe8797a2c00a74661c4bf6854d7dc",
+    "url": "/JapanesePractice/static/media/o.c83fe879.png"
+  },
+  {
+    "revision": "2c6a7093a35aeb2feca1e76f0fe1a487",
+    "url": "/JapanesePractice/static/media/pa.2c6a7093.png"
+  },
+  {
+    "revision": "7b505d3a387b23b885078dc8fdf5e03a",
+    "url": "/JapanesePractice/static/media/pa.7b505d3a.png"
+  },
+  {
+    "revision": "1d214c1f06ccffb8c46a26dc15dcbe4a",
+    "url": "/JapanesePractice/static/media/pe.1d214c1f.png"
+  },
+  {
+    "revision": "6dfff495eafd2d25bb734f7c6950be8a",
+    "url": "/JapanesePractice/static/media/pe.6dfff495.png"
+  },
+  {
+    "revision": "5b18ab0a8d828b414f199010c5615c06",
+    "url": "/JapanesePractice/static/media/pi.5b18ab0a.png"
+  },
+  {
+    "revision": "d0aa2112f2395bd0dea5776ac09a5673",
+    "url": "/JapanesePractice/static/media/pi.d0aa2112.png"
+  },
+  {
+    "revision": "393d1ef42cdf6e36fa0cf71615c69f08",
+    "url": "/JapanesePractice/static/media/po.393d1ef4.png"
+  },
+  {
+    "revision": "6f2cb7414fa5b27c7209718968aad620",
+    "url": "/JapanesePractice/static/media/po.6f2cb741.png"
+  },
+  {
+    "revision": "29b28c728fbdfbf228c3392dcfb9c7c1",
+    "url": "/JapanesePractice/static/media/pu.29b28c72.png"
+  },
+  {
+    "revision": "f9dc5995ddd1dfb5a0fea58e8899c51a",
+    "url": "/JapanesePractice/static/media/pu.f9dc5995.png"
+  },
+  {
+    "revision": "3249f2c0338bcbca3c6604ea6e7aefc5",
+    "url": "/JapanesePractice/static/media/ra.3249f2c0.png"
+  },
+  {
+    "revision": "eca4b722ced1dde8159a66da97d80d39",
+    "url": "/JapanesePractice/static/media/ra.eca4b722.png"
+  },
+  {
+    "revision": "78429f47aa873f101453a22a799f0512",
+    "url": "/JapanesePractice/static/media/re.78429f47.png"
+  },
+  {
+    "revision": "ee960ac9be864e62103163916497bed7",
+    "url": "/JapanesePractice/static/media/re.ee960ac9.png"
+  },
+  {
+    "revision": "60e963970813bf528e0014685c5de84e",
+    "url": "/JapanesePractice/static/media/ri.60e96397.png"
+  },
+  {
+    "revision": "69816e9b8907532a5ee01f804781974c",
+    "url": "/JapanesePractice/static/media/ri.69816e9b.png"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/JapanesePractice/static/media/rightArrow.5e5b72cc.svg"
+  },
+  {
+    "revision": "743a6875e84ff533d3ea3db814b7c4de",
+    "url": "/JapanesePractice/static/media/ro.743a6875.png"
+  },
+  {
+    "revision": "f8cf5bc3066420afd77aa17fa4ee9a43",
+    "url": "/JapanesePractice/static/media/ro.f8cf5bc3.png"
+  },
+  {
+    "revision": "2c9a8f15f893179ad0a254a7bdc6fc05",
+    "url": "/JapanesePractice/static/media/ru.2c9a8f15.png"
+  },
+  {
+    "revision": "bc01957523bedab0f74cc597eb25653e",
+    "url": "/JapanesePractice/static/media/ru.bc019575.png"
+  },
+  {
+    "revision": "5ef5a874c97a67042b4f669005a6d354",
+    "url": "/JapanesePractice/static/media/sa.5ef5a874.png"
+  },
+  {
+    "revision": "af3e59593814a1c67c8eb2500af9660f",
+    "url": "/JapanesePractice/static/media/sa.af3e5959.png"
+  },
+  {
+    "revision": "6c09d50b2950981a3a6e2e78da5e2682",
+    "url": "/JapanesePractice/static/media/se.6c09d50b.png"
+  },
+  {
+    "revision": "7024e05988529aa77c9e643316c1fc79",
+    "url": "/JapanesePractice/static/media/se.7024e059.png"
+  },
+  {
+    "revision": "78303f486d0cb5a33262244dd04d1b5b",
+    "url": "/JapanesePractice/static/media/shi.78303f48.png"
+  },
+  {
+    "revision": "a46fa8e4bbbc8c6e2e672d91b2bc1876",
+    "url": "/JapanesePractice/static/media/shi.a46fa8e4.png"
+  },
+  {
+    "revision": "99aa087b833dc12703f665c61ffebdaa",
+    "url": "/JapanesePractice/static/media/so.99aa087b.png"
+  },
+  {
+    "revision": "dee9918227b17aa9b786d1e80359399d",
+    "url": "/JapanesePractice/static/media/so.dee99182.png"
+  },
+  {
+    "revision": "2bfc5a84f2a4ee30f22ab05506b87249",
+    "url": "/JapanesePractice/static/media/su.2bfc5a84.png"
+  },
+  {
+    "revision": "949c8bfbf423b6bbf6b84c282eed1e0a",
+    "url": "/JapanesePractice/static/media/su.949c8bfb.png"
+  },
+  {
+    "revision": "027014db5e2999e9c072fc4cfefbfadd",
+    "url": "/JapanesePractice/static/media/ta.027014db.png"
+  },
+  {
+    "revision": "50e000437607e6349c9083fcfe053fa8",
+    "url": "/JapanesePractice/static/media/ta.50e00043.png"
+  },
+  {
+    "revision": "413599a8977b6a744bad06d7631a0ead",
+    "url": "/JapanesePractice/static/media/te.413599a8.png"
+  },
+  {
+    "revision": "ca77410046dc61ab179294553c51ffe2",
+    "url": "/JapanesePractice/static/media/te.ca774100.png"
+  },
+  {
+    "revision": "74048cafd3abfabb02a5017ea74f5611",
+    "url": "/JapanesePractice/static/media/to.74048caf.png"
+  },
+  {
+    "revision": "ae56b04528a206fbf5f724be12668953",
+    "url": "/JapanesePractice/static/media/to.ae56b045.png"
+  },
+  {
+    "revision": "8c95e2ebf8370502e75b943be9a7341d",
+    "url": "/JapanesePractice/static/media/tsu.8c95e2eb.png"
+  },
+  {
+    "revision": "ebdceee5cd155141617f5f49d82fc6c2",
+    "url": "/JapanesePractice/static/media/tsu.ebdceee5.png"
+  },
+  {
+    "revision": "0119eb883c17371971e91d00b6d8a25d",
+    "url": "/JapanesePractice/static/media/u.0119eb88.png"
+  },
+  {
+    "revision": "7f5e08615e038c3f6db0eafc913ec6f0",
+    "url": "/JapanesePractice/static/media/u.7f5e0861.png"
+  },
+  {
+    "revision": "669466c17bec5d35aa65371907f9c6fc",
+    "url": "/JapanesePractice/static/media/wa.669466c1.png"
+  },
+  {
+    "revision": "7f584fa05f7381e56b7b4ad009801c94",
+    "url": "/JapanesePractice/static/media/wa.7f584fa0.png"
+  },
+  {
+    "revision": "5ca11e50c440c8b93e4b1ecbd014c3bb",
+    "url": "/JapanesePractice/static/media/we.5ca11e50.png"
+  },
+  {
+    "revision": "4d4712b631bb2aed7d12611a693cb8f8",
+    "url": "/JapanesePractice/static/media/wi.4d4712b6.png"
+  },
+  {
+    "revision": "56a6d836072a17427eef5612fa6258a2",
+    "url": "/JapanesePractice/static/media/wo.56a6d836.png"
+  },
+  {
+    "revision": "bc10c780ce3e7c1db184c9f9bfb02143",
+    "url": "/JapanesePractice/static/media/wo.bc10c780.png"
+  },
+  {
+    "revision": "020348b98cdafa831731cc2ca1f03c53",
+    "url": "/JapanesePractice/static/media/ya.020348b9.png"
+  },
+  {
+    "revision": "471d4d7ca617ffea8f160f2aa21aef0d",
+    "url": "/JapanesePractice/static/media/ya.471d4d7c.png"
+  },
+  {
+    "revision": "22bcbce1d8e3b6e3e022198e510ef9b7",
+    "url": "/JapanesePractice/static/media/yo.22bcbce1.png"
+  },
+  {
+    "revision": "8e6d764536ef2e5dfb423ab2e8d41ac2",
+    "url": "/JapanesePractice/static/media/yo.8e6d7645.png"
+  },
+  {
+    "revision": "e8e83ce7f8df2a43bfb6a6657137835b",
+    "url": "/JapanesePractice/static/media/yu.e8e83ce7.png"
+  },
+  {
+    "revision": "f19a2b988f354d0f1789ee33522830a1",
+    "url": "/JapanesePractice/static/media/yu.f19a2b98.png"
+  },
+  {
+    "revision": "47ca8a2fdc045a6feef7983d09ecf2e5",
+    "url": "/JapanesePractice/static/media/za.47ca8a2f.png"
+  },
+  {
+    "revision": "6cca9678dd61c1124dc52dfc01e952c0",
+    "url": "/JapanesePractice/static/media/za.6cca9678.png"
+  },
+  {
+    "revision": "811e2378c00c45d25d5d6dcd61e4d8b0",
+    "url": "/JapanesePractice/static/media/ze.811e2378.png"
+  },
+  {
+    "revision": "d831327ad56a0b86494ea608624f9273",
+    "url": "/JapanesePractice/static/media/ze.d831327a.png"
+  },
+  {
+    "revision": "814cd6445ee1ee9cb25a088d6740ed4d",
+    "url": "/JapanesePractice/static/media/zo.814cd644.png"
+  },
+  {
+    "revision": "f2cea72e69a84ae9fbe51c89474194fb",
+    "url": "/JapanesePractice/static/media/zo.f2cea72e.png"
+  },
+  {
+    "revision": "7b13be11064c6a5b71690fc151c5c925",
+    "url": "/JapanesePractice/static/media/zu.7b13be11.png"
+  },
+  {
+    "revision": "895d00628092e9af52e5a9be292b0b51",
+    "url": "/JapanesePractice/static/media/zu.895d0062.png"
+  },
+  {
+    "revision": "589b6a53ada1b2d8825aab788cb49fea",
+    "url": "/JapanesePractice/static/media/zu2.589b6a53.png"
+  },
+  {
+    "revision": "d09d96bb5d750f0dec65a4a5d91abd53",
+    "url": "/JapanesePractice/static/media/zu2.d09d96bb.png"
+  }
+]);
